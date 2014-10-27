733cc410f2edc61026531858b6b65a0a27f4d80a
王瀚宇 <huw12313212@gmail.com>
2014-10-27 15:59:19 +0800

    Initial Commit

---
+++ CGHW1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CGHW1
+//
+//  Created by 王瀚宇 on 2014/10/27.
+//  Copyright (c) 2014年 王瀚宇. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
